b24724d88df5f80dfe4f83bf3440c7f91c81bb63
Schripsema <fmschripsema@gmail.com>
2022-02-02 20:54:35 +0100

    delete router module, since its not used

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostsComponent } from './views/posts/posts.component';
 import { TileComponent } from './views/shared/tile/tile.component';
@@ -15,7 +14,6 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule
   ],
   providers: [],
